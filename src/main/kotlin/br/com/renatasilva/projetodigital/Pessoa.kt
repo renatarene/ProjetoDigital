ae135791f6de1e89ae6bf38d6ef3e1b47384f5e0
Renata Silva <renatarene.silva573@gmail.com>
2021-03-12 03:38:51 -0300

    criado classe pessoa

---
+++ src/main/kotlin/br/com/renatasilva/projetodigital/Pessoa.kt
@@ -0,0 +1,14 @@
+package br.com.renatasilva.projetodigital
+
+class Pessoa {
+    var nome = "Renata"
+    var cpf = "123.123.123.12"
+
+}
+fun main() {
+    val renata = Pessoa()
+
+    println(renata.nome)
+    println(renata.cpf)
+
+}
