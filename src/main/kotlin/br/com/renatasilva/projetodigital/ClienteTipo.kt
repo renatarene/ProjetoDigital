9274d2f68e8d00de529cef59c6c9174209190b1b
Renata Silva <renatarene.silva573@gmail.com>
2021-03-15 02:38:15 -0300

    criado Enum ClienteTipo.kt e adicionado teste de exemplo de uso do Enum.

---
+++ src/main/kotlin/br/com/renatasilva/projetodigital/ClienteTipo.kt
@@ -0,0 +1,6 @@
+package br.com.renatasilva.projetodigital
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa física"),
+    PJ("Pessoa Jurídica");
+}
---
+++ src/main/kotlin/br/com/renatasilva/projetodigital/testes/ClienteTipoEnum.kt
@@ -0,0 +1,18 @@
+package br.com.renatasilva.projetodigital.testes
+
+import br.com.renatasilva.projetodigital.ClienteTipo
+
+fun main () {
+    ClienteTipo.values().forEach {elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+    val pf = ClienteTipo.PF
+    println("${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name} - ${pj.descricao}")
+
+}
