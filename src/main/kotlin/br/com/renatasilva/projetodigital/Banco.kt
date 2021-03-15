cfd539f262811d2cb7aee4a3b9094bbb60e3585d
Renata Silva <renatarene.silva573@gmail.com>
2021-03-15 01:32:03 -0300

    Criação das classes Banco e BancoTeste.

---
+++ src/main/kotlin/br/com/renatasilva/projetodigital/Banco.kt
@@ -0,0 +1,6 @@
+package br.com.renatasilva.projetodigital
+
+data class Banco(
+      val nome:String,
+      val numero:Int
+)
---
+++ src/main/kotlin/br/com/renatasilva/projetodigital/testes/BancoTeste.kt
@@ -0,0 +1,8 @@
+package br.com.renatasilva.projetodigital.testes
+
+import br.com.renatasilva.projetodigital.Banco
+
+fun main (){
+    val ProjetoDigital = Banco("DigiOne",12)
+
+}
